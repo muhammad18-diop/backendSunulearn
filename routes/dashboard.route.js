148c8a477bb49be5ae7c8d97d18d81e422c1cf6e
Muhammad Diop <nabidiop18@gmail.com>
2026-06-14 01:41:20 +0000

    ajout du dashboard

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -3,9 +3,10 @@ import { getStats } from "../controllers/dashboard.controller.js";
 import {protect} from "../middlewares/protect.middleware.js"
 import {adminOnly} from "../middlewares/adminOnly.middleware.js"
 
+
 const router = express.Router();
 
 
-router.get("/stats",  getStats);
+router.get("/stats", protect, adminOnly,  getStats);
 
 export default router;
