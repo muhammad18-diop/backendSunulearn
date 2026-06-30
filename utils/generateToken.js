478ebe75d707941c10169f5a81d0cd7fb5d45406
Muhammad Diop <nabidiop18@gmail.com>
2026-06-30 20:25:07 +0000

    changement bd

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 
-// Définition correcte de la fonction qui reçoit l'id de l'utilisateur en paramètre
+
 const generateToken = (id) => {
-    // Utilisation de la syntaxe standard en MAJUSCULES avec votre clé de secours
+    
     const secret = process.env.JWT_SECRET || 'variabletemporaireSunulearn';
 
-    // Retourne le jeton généré
+    
     return jwt.sign(
         { id },
         secret,
