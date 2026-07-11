3c45359f3879b9c65a50457a07a313324624b2d1
Muhammad Diop <nabidiop18@gmail.com>
2026-07-11 20:20:05 +0000

    changement bd supabase

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,9 @@ import { signup, login } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+
 router.post("/login", login);
 
 export default router;  
+
+const x = 8;
